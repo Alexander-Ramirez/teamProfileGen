e9b6e27dee7ce270e5606b7a414fe60ccd225297
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-29 13:51:00 -0600

    finished library code

--- library/Engineer.js
+++ library/Engineer.js
@@ -1,10 +1,19 @@
+const Employee = require("./Employee");
+
 class Engineer extends Employee{
-    constructor(name, id, email, github){
+    constructor(name, id, email, role, github){
         super(name, id, email);
+        this.role = role;
         this.github = github;
     };
 
     getRole(){
         return "Engineer";
     };
+
+    getGithub(){
+        return this.github;
+    };
 };
+
+module.exports = Engineer;
---
+++ library/Manager.js
@@ -0,0 +1,19 @@
+const Employee = require("./Employee");
+
+class Manager extends Employee{
+    constructor(name, id, email, role, officeNumber){
+        super(name, id, email);
+        this.role = role;
+        this.officeNumber = officeNumber;
+    };
+
+    getRole(){
+        return "Manager";
+    };
+
+    getGithub(){
+        return this.officeNumber;
+    };
+};
+
+module.exports = Manager;
